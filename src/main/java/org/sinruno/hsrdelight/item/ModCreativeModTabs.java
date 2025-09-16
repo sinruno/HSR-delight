7badd54d33caaa52560be0260d1dc263a10310aa
sinruno <runo970917@gmail.com>
2025-09-16 23:53:51 +0800

    貼圖

--- src/main/java/org/sinruno/hsrdelight/item/ModCreativeModTabs.java
+++ src/main/java/org/sinruno/hsrdelight/item/ModCreativeModTabs.java
@@ -1,14 +1,27 @@
 package org.sinruno.hsrdelight.item;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 import org.sinruno.hsrdelight.HSRdelight;
 
 public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, HSRdelight.MODID);
+
+    public static final RegistryObject<CreativeModeTab> HSRDELIGHT_TAB =CREATIVE_MODE_TABS.register("hsrdelight_tab",
+            ()-> CreativeModeTab.builder().icon(() -> new ItemStack(Moditems.OAK_CAKE_ROLLS.get()))
+                    .title(Component.translatable("creativetab.hsrdelight_tab"))
+                    .displayItems((pParameters, pOutput) ->{
+                        pOutput.accept(Moditems.OAK_CAKE_ROLLS.get());
+                        pOutput.accept(Moditems.MUNG_BEAN_SODA.get());
+                    } )
+                    .build());
+
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
     }

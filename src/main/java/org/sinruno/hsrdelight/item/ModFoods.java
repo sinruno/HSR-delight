5000b891e7f8dbbd32efd9273490e30abae1c42a
sinruno <runo970917@gmail.com>
2025-09-17 23:48:59 +0800

    蘇打豆汁效果

--- src/main/java/org/sinruno/hsrdelight/item/ModFoods.java
+++ src/main/java/org/sinruno/hsrdelight/item/ModFoods.java
@@ -2,13 +2,14 @@ package org.sinruno.hsrdelight.item;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraftforge.event.entity.living.MobEffectEvent;
 
 public class ModFoods {
     public static final FoodProperties OAK_CAKE_ROLLS = new FoodProperties.Builder().nutrition(8).saturationMod(0.3f).build();
     public static final FoodProperties MUNG_BEAN_SODA = new FoodProperties.Builder().nutrition(8).saturationMod(0.3f)
-            .effect((->new MobEffectInstance(MobEffect))).build();
+            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST,1200),1).build();
     public static final FoodProperties ABUNDANCE_OF_LUCK = new FoodProperties.Builder().nutrition(8).saturationMod(0.3f).build();
     public static final FoodProperties BERRYPHEASANT_SKEWERS = new FoodProperties.Builder().nutrition(8).saturationMod(0.3f).build();
     public static final FoodProperties IMMORTALS_DELIGHT = new FoodProperties.Builder().nutrition(8).saturationMod(0.3f).build();
